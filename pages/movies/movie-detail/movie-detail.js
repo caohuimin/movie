1f15fc26a458e190b2b711490d7da27dea3ff1d0
cindy <1037528101@qq.com>
2019-01-15 23:28:49 +0800

    增加电影详情页

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -21,7 +21,41 @@ Page({
   },
 
   processDoubanData:function(data){
-  console.log(data)
+    if(!data){
+      return;
+    }
+   var director = {
+     avator:"",
+     name:"",
+     id:""
+   }
+   if(data.directors[0]!=null){
+     if(data.directors[0].avator!=null){
+       director.avator = data.directors[0].avator.large
+     }
+     director.name = data.directors[0].name;
+     director.id = data.directors[0].id;
+   }
+   var movie = {
+     movieImg :data.images?data.images.large:"",
+     country:data.countries[0],
+     title:data.title,
+     originalTitle:data.original_title,
+     wishCount:data.wish_count,
+     commentCount:data.comments_count,
+     year:data.year,
+     generes:data.genres.join(" 、"),
+     stars:util.convertToStarsArray(data.rating.stars),
+     score:data.rating.average,
+     director:director,
+     casts:util.convertToCastString(data.casts),
+     castInfo:util.convertToCastInfos(data.casts),
+     summary:data.summary
+   }
+   console.log(movie)
+   this.setData({
+     movie:movie
+   })
   },
 
   /**
